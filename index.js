bb23c9d7c0ed94349bf4dbaa7136e4176941297b
renanadmiral <49658142+renanadmiral@users.noreply.github.com>
2021-03-12 17:31:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+//Este arquivo é responsável por subir o servidor e conectar ao banco de dados
+const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
+
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    }
+    else {
+        console.log('conectado com sucesso')
+
+        Tabelas.init(conexao)
+
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
+
+
