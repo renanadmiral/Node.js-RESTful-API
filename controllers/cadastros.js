d9d50ab6fb5ac95744b0d28796f9780abf0bdaad
renanadmiral <49658142+renanadmiral@users.noreply.github.com>
2021-04-03 19:22:17 -0300

    Update cadastros.js

--- controllers/cadastros.js
+++ controllers/cadastros.js
@@ -6,7 +6,7 @@ module.exports = app => {
         Pessoas.lista(res)
     })
 
-    app.get('/pessoas/:cpf/enderecos', (req, res) => {
+    app.get('/cadastros/pessoas/:cpf', (req, res) => {
        const cpf = req.params.cpf
 
        Pessoas.buscaPorCpf(cpf, res)
@@ -18,39 +18,39 @@ module.exports = app => {
         Pessoas.adicionaPessoa(pessoas, res)
     })
 
-    app.post('/cadastros/pessoas/enderecos', (req, res) => {
+    app.post('/cadastros/enderecos', (req, res) => {
         const enderecos = req.body
 
         Pessoas.adicionaEndereco(enderecos, res)
     })
 
-    app.post('/cadastros/pessoas/enderecos/cidades', (req, res) => {
+    app.post('/cadastros/cidades', (req, res) => {
         const cidades = req.body
 
         Pessoas.adicionaCidade(cidades, res)
     })
 
-    app.post('/cadastros/pessoas/enderecos/estados', (req, res) => {
+    app.post('/cadastros/estados', (req, res) => {
         const estados = req.body
 
         Pessoas.adicionaEstado(estados, res)
     })
 
-    app.post('/cadastros/pessoas/enderecos/paises', (req, res) => {
+    app.post('/cadastros/paises', (req, res) => {
         const paises = req.body
 
         Pessoas.adicionaPais(paises, res)
     })
 
 
-    app.patch('/cadastros/:cpf', (req, res) => {
+    app.patch('/cadastros/pessoas/:cpf', (req, res) => {
         const cpf = req.params.cpf
         const valores = req.body
 
        Pessoas.altera(cpf, valores, res)
     })
 
-    app.delete('/cadastros/:cpf', (req, res) => {
+    app.delete('/cadastros/pessoas/:cpf', (req, res) => {
         const cpf = req.params.cpf
 
         Pessoas.deleta(cpf, res)
